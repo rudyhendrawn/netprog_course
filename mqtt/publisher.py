fb807425b46875bf734044e9c0a08cd71b0e8a60
cornflake15 <rudyhend@gmail.com>
2020-04-20 20:57:53 +0800

    new code

---
+++ mqtt/publisher.py
@@ -0,0 +1,41 @@
+# Thanks to: https://techtutorialsx.com/2017/04/14/python-publishing-messages-to-mqtt-topic/
+"""Publish a Topic to MQTT Broker"""
+
+import paho.mqtt.client as mqttClient
+import time
+ 
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print('Connected to broker')
+        global Connected                #Use global variable
+        Connected = True                #Signal connection 
+    else:
+        print('Connection failed')
+ 
+Connected = False   #global variable for the state of the connection
+
+# After you successfully the publisher and subscribe code, 
+# try to create your own MQTT broker by create an account at cloudmqtt.com
+broker_address= 'm12.cloudmqtt.com'
+port = 12075
+user = 'dbkomngk'
+password = 'iWr3bpLbrojb'
+ 
+client = mqttClient.Client('Python')               #create new instance
+client.username_pw_set(user, password=password)    #set username and password
+client.on_connect= on_connect                      #attach function to callback
+client.connect(broker_address, port=port)          #connect to broker
+ 
+client.loop_start()        #start the loop
+ 
+while Connected != True:    #Wait for connection
+    time.sleep(0.1)
+ 
+try:
+    while True: 
+        value = input('Enter the message:')
+        client.publish('python/', value)
+ 
+except KeyboardInterrupt:
+    client.disconnect()
+    client.loop_stop()
